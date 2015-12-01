8515cd31985bc8e76f9406acc5450e979a479b93
root <root@panyao.(none)>
2015-12-02 07:14:01 +0800

    [CHG]add wifi_run.sh

--- run/wifi_run.sh
+++ run/wifi_run.sh
@@ -1,2 +1,22 @@
 #!/bin/sh
 
+if [ "z""$SSID" == "z" -o "z""$PSK" == "z" ]; then        
+	SSID='"@@@@@@@@"'
+        PSK='"1234567890"'
+fi
+
+if [ "z""$NAME" == "z" ]; then        
+	NAME=centos
+fi
+killall wpa_supplicant
+killall dhclient
+
+wpa_supplicant -c /etc/wpa_supplicant/wpa_supplicant.conf -i ra0 -B
+wpa_cli remove_network 0
+wpa_cli ap_scan 1
+wpa_cli add_network
+wpa_cli set_network 0 ssid $SSID
+wpa_cli set_network 0 psk $PSK
+wpa_cli select_network 0
+wpa_cli list_networks
+dhclient ra0
